272c33f61dee92072669658b4fc89fe9312e08b3
Alina Christenbury <alina.christenbury@gmail.com>
2021-03-22 09:39:37 -0400

    a few more manually-created videos

--- synthetic-video-generator.py
+++ synthetic-video-generator.py
@@ -1,3 +1,4 @@
+import cv2
 import numpy as np
 from cv2 import VideoWriter, VideoWriter_fourcc
 
@@ -9,19 +10,22 @@ radius = 150
 paint_h = int(height / 2)
 
 fourcc = VideoWriter_fourcc(*'MP42')
-filename = './data/red-black-seizure-trigger.avi'
+filename = './data/green-red-seizure-trigger.avi'
 video = VideoWriter(filename, fourcc, float(FPS), (width, height))
 
 red = (0, 0, 255)
+blue = (255, 0, 0)
+green = (0, 255, 0)
 black = (0, 0, 0)
+white = (255, 255, 255)
 
 for _ in range(FPS * seconds):
     frame = np.zeros((height, width, 3), np.uint8)
 
     if _ % 2 == 0:
-        frame[:, 0:width] = red
+        frame[:, 0:width] = green
     else:
-        frame[:, 0:width] = black
+        frame[:, 0:width] = red
 
     video.write(frame)
 
