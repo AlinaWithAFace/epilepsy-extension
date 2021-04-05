9c166cdccb4631733b0cddb2e8fa80339f9fab61
Alina Christenbury <alina.christenbury@gmail.com>
2021-04-05 10:14:49 -0400

    slightly improved but still broken stripes

--- src/generator/synthetic-video-generator.py
+++ src/generator/synthetic-video-generator.py
@@ -59,6 +59,102 @@ patterns = [solid, vertical_stripes, horizontal_stripes]
 pattern = 0  # the index of the pattern to use from the set in patterns
 
 
+def draw_stripes(frame, color1, color2):
+    # print(color1)
+    # print(color2)
+
+    # for i in range(0, 9):
+    #     if _ % 2 == 0:
+    #         cv2.rectangle(frame,
+    #                       (0, int(i * .1 * height)),
+    #                       (width, int(i + 1 * .1 * height)),
+    #                       color1,
+    #                       -1,
+    #                       8,
+    #                       0)
+    #     else:
+    #         cv2.rectangle(frame,
+    #                       (0, int(i * height)),
+    #                       (width, int(i + 1 * height)),
+    #                       color2,
+    #                       -1,
+    #                       8,
+    #                       0)
+
+    cv2.rectangle(frame,
+                  (0, int(0 * height)),
+                  (width, int(.1 * height)),
+                  colors[0][0],
+                  -1,
+                  8,
+                  0)
+    cv2.rectangle(frame,
+                  (0, int(.1 * height)),
+                  (width, int(.2 * height)),
+                  colors[1][0],
+                  -1,
+                  8,
+                  0)
+    cv2.rectangle(frame,
+                  (0, int(.2 * height)),
+                  (width, int(.3 * height)),
+                  colors[0][0],
+                  -1,
+                  8,
+                  0)
+    cv2.rectangle(frame,
+                  (0, int(.2 * height)),
+                  (width, int(.3 * height)),
+                  colors[1][0],
+                  -1,
+                  8,
+                  0)
+    cv2.rectangle(frame,
+                  (0, int(.3 * height)),
+                  (width, int(.4 * height)),
+                  colors[0][0],
+                  -1,
+                  8,
+                  0)
+    cv2.rectangle(frame,
+                  (0, int(.4 * height)),
+                  (width, int(.5 * height)),
+                  colors[1][0],
+                  -1,
+                  8,
+                  0)
+    cv2.rectangle(frame,
+                  (0, int(.5 * height)),
+                  (width, int(.6 * height)),
+                  colors[0][0],
+                  -1,
+                  8,
+                  0)
+    cv2.rectangle(frame,
+                  (0, int(.6 * height)),
+                  (width, int(.7 * height)),
+                  colors[1][0],
+                  -1,
+                  8,
+                  0)
+    cv2.rectangle(frame,
+                  (0, int(.7 * height)),
+                  (width, int(.8 * height)),
+                  colors[0][0],
+                  -1,
+                  8,
+                  0)
+    cv2.rectangle(frame,
+                  (0, int(.8 * height)),
+                  (width, int(.9 * height)),
+                  colors[1][0],
+                  -1,
+                  8,
+                  0)
+
+    return frame
+
+
 def generate_video():
     seconds = random.randint(min_length, max_length)
 
@@ -78,36 +174,26 @@ def generate_video():
     video = cv2.VideoWriter(filename, fourcc, float(FPS), (width, height))
 
     # if pattern == 1:
-    #     for _ in range(FPS * seconds):
-    #         frame = np.zeros((height, width, 3), np.uint8)
-    #
-    #         x = (0, 0)
-    #         y = (1, 1)
-    #         cv2.rectangle(frame,
-    #                       x,
-    #                       y,
-    #                       colors[0][0],
-    #                       1,
-    #                       cv2.FILLED,
-    #                       0)
-    #
-    #         # if _ % 2 == 0:
-    #         #     frame[:, 0:width] = colors[0][0]
-    #         # else:
-    #         #     frame[:, 0:width] = colors[1][0]
-    #
-    #         video.write(frame)
-    #
-    # else:
     for _ in range(FPS * seconds):
         frame = np.zeros((height, width, 3), np.uint8)
 
-        if _ % 2 == 0:
-            frame[:, 0:width] = colors[0][0]
-        else:
-            frame[:, 0:width] = colors[1][0]
+        video.write(frame)
+
+        # frame = draw_stripes(frame, colors[0][0], colors[1][0])
+        frame = draw_stripes(frame, red[0], green[0])
 
         video.write(frame)
+    #
+    # else:
+    # for _ in range(FPS * seconds):
+    #     frame = np.zeros((height, width, 3), np.uint8)
+    #
+    #     if _ % 2 == 0:
+    #         frame[:, 0:width] = colors[0][0]
+    #     else:
+    #         frame[:, 0:width] = colors[1][0]
+    #
+    #     video.write(frame)
 
     video.release()
 
