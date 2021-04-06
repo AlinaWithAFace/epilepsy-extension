faeb7d31438ee317b86986c30afc7ef945688019
Connor Onweller <onweller@udel.edu>
2021-04-05 20:13:50 -0400

    Better error handling

--- src/server/routes/videos.py
+++ src/server/routes/videos.py
@@ -1,10 +1,48 @@
 import pafy
 import http
-import sqlite3
 import database
-from flask import Blueprint
+import json
+from flask import Blueprint, Response
+from sqlite3 import IntegrityError
 
 blueprint = Blueprint("videos", __name__)
+
+
+@blueprint.route("/<id>", methods=["GET"])
+def get_video_by_id(id):
+    """Retrieves a video resource via it's id
+
+    Input:
+
+    -   `id`: integer video identifier
+
+    Returns:
+
+    Dictionary data of the retrieved resource and a http status code
+    """
+
+    response = Response()
+
+    with database.connection() as connection:
+        cursor = connection.cursor()
+
+        cursor.execute(
+            """
+            SELECT video_id, video_vid, video_title FROM Videos
+            WHERE video_id = ?
+            """,
+            (id,),
+        )
+
+        row = cursor.fetchone()
+
+        if not row:
+            response.status_code = http.HTTPStatus.NOT_FOUND
+        else:
+            response.data = json.dumps(dict(row))
+            response.status_code = http.HTTPStatus.OK
+
+        return response
 
 
 @blueprint.route("/vid/<vid>", methods=["GET"])
@@ -13,12 +51,12 @@ def get_video_by_vid(vid):
 
     Input:
 
-    -   `vid`: unique YouTube video identifier
-
-    Returns:
+    -   `vid`: unique YouTube video identifier Returns:
 
     Dictionary data of the retrieved resource and a http status code
     """
+
+    response = Response()
 
     with database.connection() as connection:
         cursor = connection.cursor()
@@ -34,9 +72,12 @@ def get_video_by_vid(vid):
         row = cursor.fetchone()
 
         if not row:
-            return {}, http.HTTPStatus.NOT_FOUND
+            response.status_code = http.HTTPStatus.NOT_FOUND
+        else:
+            response.data = json.dumps(dict(row))
+            response.status_code = http.HTTPStatus.OK
 
-        return dict(row), http.HTTPStatus.OK
+        return response
 
 
 @blueprint.route("/vid/<vid>", methods=["POST"])
@@ -52,18 +93,32 @@ def create_video_by_vid(vid):
     Dictionary data of the created resource and a http status code
     """
 
+    response = Response()
+
     try:
         video = pafy.new(vid)
-    except:
-        return {}, http.HTTPStatus.BAD_REQUEST
 
-    with database.connection() as connection:
-        cursor = connection.cursor()
+        with database.connection() as connection:
+            cursor = connection.cursor()
 
-        cursor.execute(
-            """
-            INSERT INTO Videos (video_vid, video_title)
-            VALUES (?, ?)
-            """,
-            (video.videoid, video.title),
-        )
+            cursor.execute(
+                """
+                INSERT INTO Videos (video_vid, video_title)
+                VALUES (?, ?);
+                """,
+                (video.videoid, video.title),
+            )
+            new_id = cursor.lastrowid
+            connection.commit()
+
+        response.headers["Content-Location"] = f"/videos/{new_id}"
+        response.headers["Location"] = f"/videos/{new_id}"
+        response.status_code = http.HTTPStatus.CREATED
+        return response
+
+    except ValueError:
+        response.status_code = http.HTTPStatus.BAD_REQUEST
+        return response
+    except IntegrityError:
+        response.status_code = http.HTTPStatus.BAD_REQUEST
+        return response
