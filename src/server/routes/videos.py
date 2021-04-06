f5c0a281b73d0b478cf5a4e756f79fc6b2ae38f5
Connor Onweller <onweller@udel.edu>
2021-04-05 22:45:17 -0400

    Added warning routes

--- src/server/routes/videos.py
+++ src/server/routes/videos.py
@@ -10,115 +10,70 @@ blueprint = Blueprint("videos", __name__)
 
 @blueprint.route("/<id>", methods=["GET"])
 def get_video_by_id(id):
-    """Retrieves a video resource via it's id
+    """Retrieves a video resource via it's identifier"""
 
-    Input:
-
-    -   `id`: integer video identifier
-
-    Returns:
-
-    Dictionary data of the retrieved resource and a http status code
-    """
-
-    response = Response()
-
-    with database.connection() as connection:
-        cursor = connection.cursor()
-
-        cursor.execute(
-            """
-            SELECT video_id, video_vid, video_title FROM Videos
+    cursor = database.execute(
+        """
+        SELECT video_id, video_vid, video_title FROM Videos
             WHERE video_id = ?
-            """,
-            (id,),
-        )
+        """,
+        (id,),
+    )
 
-        row = cursor.fetchone()
+    row = cursor.fetchone()
 
-        if not row:
-            response.status_code = http.HTTPStatus.NOT_FOUND
-        else:
-            response.data = json.dumps(dict(row))
-            response.status_code = http.HTTPStatus.OK
-
-        return response
+    if not row:
+        return Response(status=http.HTTPStatus.NOT_FOUND)
+    else:
+        return Response(response=json.dumps(dict(row)), status=http.HTTPStatus.OK)
 
 
 @blueprint.route("/vid/<vid>", methods=["GET"])
 def get_video_by_vid(vid):
-    """Retrieves a video resource via a YouTube video identifier
+    """Retrieves a video resource via a YouTube video identifier"""
 
-    Input:
+    cursor = database.execute(
+        """
+        SELECT video_id, video_vid, video_title FROM Videos
+        WHERE video_vid = ?
+        """,
+        (vid,),
+    )
 
-    -   `vid`: unique YouTube video identifier Returns:
+    row = cursor.fetchone()
 
-    Dictionary data of the retrieved resource and a http status code
-    """
-
-    response = Response()
-
-    with database.connection() as connection:
-        cursor = connection.cursor()
-
-        cursor.execute(
-            """
-            SELECT video_id, video_vid, video_title FROM Videos
-            WHERE video_vid = ?
-            """,
-            (vid,),
-        )
-
-        row = cursor.fetchone()
-
-        if not row:
-            response.status_code = http.HTTPStatus.NOT_FOUND
-        else:
-            response.data = json.dumps(dict(row))
-            response.status_code = http.HTTPStatus.OK
-
-        return response
+    if not row:
+        return Response(status=http.HTTPStatus.NOT_FOUND)
+    else:
+        return Response(response=json.dumps(dict(row)), status=http.HTTPStatus.OK)
 
 
 @blueprint.route("/vid/<vid>", methods=["POST"])
 def create_video_by_vid(vid):
-    """Creates a video resource
-
-    Input:
-
-    -   `vid`: unique YouTube video identifier
-
-    Returns:
-
-    Dictionary data of the created resource and a http status code
-    """
+    """Creates a video resource using a YouTube video identifier"""
 
     response = Response()
 
     try:
         video = pafy.new(vid)
 
-        with database.connection() as connection:
-            cursor = connection.cursor()
+        cursor = database.execute(
+            """
+            INSERT INTO Videos (video_vid, video_title)
+            VALUES (?, ?);
+            """,
+            (video.videoid, video.title),
+            commit=True,
+        )
+        new_id = cursor.lastrowid
 
-            cursor.execute(
-                """
-                INSERT INTO Videos (video_vid, video_title)
-                VALUES (?, ?);
-                """,
-                (video.videoid, video.title),
-            )
-            new_id = cursor.lastrowid
-            connection.commit()
+        return Response(
+            headers={
+                "Location": f"/videos/{new_id}",
+            },
+            status_code=http.HTTPStatus.CREATED
+        )
 
-        response.headers["Content-Location"] = f"/videos/{new_id}"
-        response.headers["Location"] = f"/videos/{new_id}"
-        response.status_code = http.HTTPStatus.CREATED
-        return response
-
-    except ValueError:
-        response.status_code = http.HTTPStatus.BAD_REQUEST
-        return response
-    except IntegrityError:
-        response.status_code = http.HTTPStatus.BAD_REQUEST
-        return response
+    except (ValueError, IntegrityError, OSError) as e:
+        print(e)
+        return Response(status=http.HTTPStatus.BAD_REQUEST)
---
+++ src/server/routes/warnings.py
@@ -0,0 +1,121 @@
+import database
+import http
+import json
+from flask import Blueprint, Response, request
+from sqlite3 import IntegrityError
+
+
+blueprint = Blueprint("warnings", __name__)
+
+
+@blueprint.route("/<video_id>/warnings/<warning_id>", methods=["GET"])
+def get_warning(video_id, warning_id):
+    cursor = database.execute(
+        """
+        SELECT warning_id, warning_created, warning_start, warning_end,
+               warning_msg, warning_source
+        FROM Warnings
+        WHERE warning_video_id = ?
+        AND
+        warning_id = ?
+        """,
+        (video_id, warning_id),
+    )
+
+    row = cursor.fetchone()
+
+    if row is None:
+        return Response(status=http.HTTPStatus.NOT_FOUND)
+    else:
+        return Response(response=json.dumps(dict(row)), status=http.HTTPStatus.OK)
+
+
+@blueprint.route("/<video_id>/warnings", methods=["GET"])
+def get_warnings(video_id):
+    """Gets all warnings for a video. Can pass a query parameter `source` to
+    specify computer or user generated warnings.
+    """
+
+    warning_source = request.args.get("source", default=None)
+
+    if not warning_source:
+        cursor = database.execute(
+            """
+            SELECT warning_id, warning_created, warning_start, warning_end,
+                   warning_msg, warning_source
+            FROM Warnings
+            WHERE warning_video_id = ?
+            """,
+            (video_id,),
+        )
+    elif warning_source not in ("AUTO", "USER"):
+        return Response(status=http.HTTPStatus.BAD_REQUEST)
+    else:
+        cursor = database.execute(
+            """
+            SELECT warning_id, warning_created, warning_start, warning_end,
+                   warning_msg, warning_source
+            FROM Warnings
+            WHERE warning_video_id = ?
+                  AND
+                  warning_source = ?
+            """,
+            (video_id, warning_source),
+        )
+
+    rows = cursor.fetchall()
+
+    if rows is None:
+        return Response(status=http.HTTPStatus.NOT_FOUND)
+    else:
+        return Response(
+            response=json.dumps(list(map(dict, rows))), status=http.HTTPStatus.OK
+        )
+
+    return response
+
+
+@blueprint.route("/<video_id>/warnings", methods=["POST"])
+def create_warning(video_id):
+    """Create user generated warning for the video"""
+
+    try:
+        request_data = request.get_json()
+
+        if not request_data or any(
+            x not in request_data for x in ("start", "stop", "message")
+        ):
+            print("missing field", request_data)
+            return Response(status=http.HTTPStatus.BAD_REQUEST)
+        start = request_data["start"]
+        stop = request_data["stop"]
+        message = request_data["message"]
+
+        cursor = database.execute(
+            """
+            INSERT INTO Warnings (warning_video_id, warning_start, warning_end,
+            warning_msg, warning_source)
+            VALUES (?, ?, ?, ?, "USER")
+            """,
+            (video_id, start, stop, message),
+            commit=True,
+        )
+
+        new_id = cursor.lastrowid
+
+        return Response(
+            headers={
+                "Location": f"/videos/{video_id}/warnings/{new_id}",
+            },
+            status=http.HTTPStatus.CREATED,
+        )
+
+    except (IntegrityError) as e:
+        print(e)
+        return Response(status=http.HTTPStatus.BAD_REQUEST)
+
+
+@blueprint.route("/<video_id>/warnings/generate", methods=["POST"])
+def generate_warnings(video_id):
+    """Generate warnings for video"""
+    return Response(status=http.HTTPStatus.NOT_IMPLEMENTED)
