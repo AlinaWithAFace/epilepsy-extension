0751b90e1f36697cf097297a55df4411fcbd1dbf
Connor Onweller <onweller@udel.edu>
2021-05-13 12:04:31 -0400

    Make buttons look more button-ish

--- src/extension/index.js
+++ src/extension/index.js
@@ -1,8 +1,14 @@
 window.onload = () => {
-  chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
-    var app = Elm.Main.init({
-      node: document.getElementById("myapp"),
-      flags: tabs[0].url
-    });
+  var app = Elm.Main.init({
+    node: document.getElementById("myapp"),
+    flags: "https://www.youtube.com/watch?v=QRZqC1tr9KE"
   });
 };
+// window.onload = () => {
+//   chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
+//     var app = Elm.Main.init({
+//       node: document.getElementById("myapp"),
+//       flags: tabs[0].url
+//     });
+//   });
+// };
