bcef62bdb44aea78ce7c069e8c24a62e49c61934
Connor Onweller <onweller@udel.edu>
2021-05-13 12:04:56 -0400

    Fix js problem

--- src/extension/index.js
+++ src/extension/index.js
@@ -1,14 +1,8 @@
 window.onload = () => {
-  var app = Elm.Main.init({
-    node: document.getElementById("myapp"),
-    flags: "https://www.youtube.com/watch?v=QRZqC1tr9KE"
+  chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
+    var app = Elm.Main.init({
+      node: document.getElementById("myapp"),
+      flags: tabs[0].url
+    });
   });
 };
-// window.onload = () => {
-//   chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
-//     var app = Elm.Main.init({
-//       node: document.getElementById("myapp"),
-//       flags: tabs[0].url
-//     });
-//   });
-// };
